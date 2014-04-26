3fbbd374da7713b06bf8262db50b8947ddfa1d82
juanpemb <juanpemb@gmail.com>
2014-04-26 10:21:49 +0200

    creo proyecto en repo

---
+++ dx-downloader/src/com/jp/api/Articulo.java
@@ -0,0 +1,13 @@
+package com.jp.api;
+
+import java.io.Serializable;
+
+public class Articulo implements Serializable {
+
+	private Integer codigo;
+	private String nombre;
+	private String subtitulo;
+	private String descripcion;
+	private Integer precio;
+
+}
